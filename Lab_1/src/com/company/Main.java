e8eea237cc589432be6cc1fab4617020a718e91c
Masterex395 <73644602+Masterex395@users.noreply.github.com>
2021-10-11 19:19:45 +0200

    Add files via upload

---
+++ Lab_1/src/com/company/Main.java
@@ -0,0 +1,125 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Zad 1
+        java.lang.System.out.println(31 + 29 + 31);
+
+        //Zad 2
+        int suma = 0;
+        for (int i = 0; i < 11; i++)
+        {
+            suma += i;
+        }
+        java.lang.System.out.println(suma);
+
+        //Zad 3
+        int suma2 = 1;
+        for (int i = 1; i < 11; i++)
+        {
+            suma2 *= i;
+        }
+        java.lang.System.out.println(suma2);
+
+        //Zad 4
+        int kasa = 1000;
+        for (int i = 0; i < 4; i++)
+        {
+            kasa *= 1.06;
+        }
+        java.lang.System.out.println(kasa);
+
+        //Zad 5
+        java.lang.System.out.println("------\n" + "|Java|\n" + "------\n");
+
+        //Zad 6
+        java.lang.System.out.println
+                ("  /////\n" +
+                " +\"\"\"\"\"+\n" +
+                "(| o o |)\n" +
+                "  | ^ |\n" +
+                " | `-' |\n" +
+                " +-----+\n");
+
+        //Zad 7
+        java.lang.System.out.println(
+                "*         *       *      ******* *****  *    *   *****   *****      ******  *****   *****  *****  ***** \n" +
+                "**       **      * *        *    *      *    *    *          *      *       *   *      *   *       *    \n" +
+                "* *     * *     *   *       *    *      *    *     *        *       *       *****     *    *        *   \n" +
+                "*  *   *  *    * *** *      *    ***    *    *      *      *        *  ***  * *      *     ***       *  \n" +
+                "*   * *   *   *       *     *    *      *    *       *    *         *    *  *  *    *      *          * \n" +
+                "*    *    *  *         *    *    *****  ******    *****  *****      ******  *   *  *****   *****   *****\n");
+
+        //Zad 8
+        java.lang.System.out.println
+                ("   +\n" +
+                "  + +\n" +
+                " +   +\n" +
+                "+-----+\n" +
+                "| .-. |\n" +
+                "| | | |\n" +
+                "+-+-+-+\n");
+
+        //Zad 9
+        java.lang.System.out.println
+                ("                         ------\n" +
+                "    _____              /  Hello \\\n" +
+                "  /       \\_||_      <  Junior   >\n" +
+                " /           :_|       \\ Coder! /\n" +
+                " \\__________/            ------\n"
+                );
+
+        //Zad 10
+        java.lang.System.out.println("Szybcy i Wsciekli\n" + "Wikingowie\n" + "Squid Game\n");
+
+        //Zad 11
+        java.lang.System.out.println
+                ("Wisława Szymborska - Perpektywa\n\n"+
+                 "        Minęli się jak obcy,\n" +
+                 "        bez gestu i słowa,\n" +
+                 "        ona w drodze do sklepu,\n" +
+                 "        on do samochodu.\n" +
+                 "         \n" +
+                 "        Może w popłochu\n" +
+                 "        albo roztargnieniu\n" +
+                 "        albo niepamiętaniu,\n" +
+                 "        że przez krótki czas\n" +
+                 "        kochali się na zawsze.\n" +
+                 "\n" +
+                 "        Nie ma zresztą gwarancji,\n" +
+                 "        że to byli oni.\n" +
+                 "        Może z daleka tak,\n" +
+                 "        a z bliska wcale.\n" +
+                 "\n" +
+                 "        Zobaczyłam ich z okna,\n" +
+                 "        a kto patrzy z góry,\n" +
+                 "        ten najłatwiej się myli.\n" +
+                 "\n" +
+                 "        Ona zniknęła za szklanymi drzwiami,\n" +
+                 "        on siadł za kierownicą\n" +
+                 "        i szybko odjechał.\n" +
+                 "        Czyli nic się nie stało\n" +
+                 "        nawet jeśli stało.\n" +
+                 "\n" +
+                 "        A ja, tylko przez moment\n" +
+                 "        pewna, co widziałam,\n" +
+                 "        próbuję teraz w przygodnym wierszyku\n" +
+                 "        wmawiać Wam, Czytelnikom,\n" +
+                 "        że to było smutne. \n"
+                 );
+
+        //Zad 12
+        java.lang.System.out.println
+                ("******************================================\n" +
+                "*******************===============================\n" +
+                "*******************===============================\n" +
+                "*******************===============================\n" +
+                "*******************===============================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================\n" +
+                "==================================================\n");
+    }
+}
